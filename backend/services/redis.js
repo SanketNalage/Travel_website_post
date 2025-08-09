ffbca460f0a4b9d248a60a5f715f98382b735788
SanketNalage <sanketnalage882gmail.com>
2025-08-09 17:42:02 +0530

    Added the file and foler in project

--- backend/services/redis.js
+++ backend/services/redis.js
@@ -4,21 +4,21 @@ import { REDIS_URL } from '../config/utils.js';
 let redis = null;
 
 export async function connectToRedis() {
-  try {
-    if (REDIS_URL) {
-      redis = await createClient({
-        url: REDIS_URL,
-        disableOfflineQueue: true,
-      }).connect();
-      console.log('Redis Connected: ' + REDIS_URL);
-    } else {
-      console.log('Redis not configured, cache disabled.');
+    try {
+        if (REDIS_URL) {
+            redis = await createClient({
+                url: REDIS_URL,
+                disableOfflineQueue: true,
+            }).connect();
+            console.log('Redis Connected: ' + REDIS_URL);
+        } else {
+            console.log('Redis not configured, cache disabled.');
+        }
+    } catch (error) {
+        console.error('Error connecting to Redis:', error.message);
     }
-  } catch (error) {
-    console.error('Error connecting to Redis:', error.message);
-  }
 }
 
 export function getRedisClient() {
-  return redis;
+    return redis;
 }
